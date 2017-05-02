0865e05c2b5b20e940aaac82a2552f998eba94dc
Nihaal Modi <nihaalmodi53@gmail.com>
2017-05-02 16:21:06 +0000

    model_made

--- webportal/class/models.py
+++ webportal/class/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class timetable(models.Model):
+    subject_number = models.CharField(max_length=10)
+    room = models.CharField(max_length=5)
+    day = models.CharField(max_length=10)
+    start_time = models.IntegerField('Enter time in 24-hour format',max_length=2)
+    end_time = models.IntegerField('Enter time in 24-hour format',max_length=2)
+    prof = models.CharField('Enter proffessor name',max_length=20)
+    def __str__(self):
+        return (self.subject_number+' '+self.prof+' '+self.day)
